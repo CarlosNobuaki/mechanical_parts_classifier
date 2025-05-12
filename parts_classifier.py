f653477d709245be6f6b17067c9776ba6bbb1eba
carlos.nobuaki <carlos.nobuaki@ciag.org.br>
2025-05-12 16:58:27 -0300

    feat: img_cut

--- parts_classifier.py
+++ parts_classifier.py
@@ -4,7 +4,7 @@ from PIL import Image
 from ultralytics import YOLO
 
 #st.image("/home/carlos/workspace/synapse/mechanical_parts_classifier/mechanical_parts_classifier/img/Captura de tela de 2025-05-11 20-33-07.png", use_container_width=True)# Título da aplicação
-st.title("Agroamerica classificador de Peças")
+st.title("Agromerica classificador de Peças")
 #--- Descrição da aplicação
 # --- Links úteis ---
 st.markdown("---")
