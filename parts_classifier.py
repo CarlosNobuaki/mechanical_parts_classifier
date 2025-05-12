71451e0e8853732fe3fa0d66bb9c9f74848023fe
carlos.nobuaki <carlos.nobuaki@ciag.org.br>
2025-05-11 21:33:55 -0300

    feat: requirements

--- parts_classifier.py
+++ parts_classifier.py
@@ -3,7 +3,6 @@ import numpy as np
 from PIL import Image
 from ultralytics import YOLO
 
-st.image("/home/carlos/workspace/synapse/mechanical_parts_classifier/mechanical_parts_classifier/img/Captura de tela de 2025-05-11 20-33-07.png", use_container_width=True)
 # Título da aplicação
 st.title("Classificador de Peças")
 
